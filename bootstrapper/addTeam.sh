8eead7e0f78139a0e1582c01f0c142973b1355af
Simon Basset <simon.basset@gmail.com>
2018-03-06 12:11:40 +0100

    chore(quality): improve bash quality
    adds shellcheck

--- bootstrapper/addTeam.sh
+++ bootstrapper/addTeam.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-set -e
+addTeam() {
+    local url=$1
+    local name=$2
+    local team=$3
+    local mail=$4
+    local credentials=$5
+    local password=$6
+    local token=''
 
-while getopts "u:n:t:m:c:p:" opt; do
-  case $opt in
-    u) URL="$OPTARG";;
-    n) NAME="$OPTARG";;
-    t) TEAM="$OPTARG";;
-    m) MAIL="$OPTARG";;
-    c) CREDENTIALS="$OPTARG";;
-    p) PASSWORD="$OPTARG";;
-  esac
-done
+    token=$(curl -b cookies.txt --cookie-jar cookies.txt -s -L "${url}/login" | pup '[name=nonce] attr{value}')
+    curl -s -o /dev/null -b cookies.txt -c cookies.txt -F "nonce=${token}" -F "name=${name}" -F "password=${password}" -H "Expect:" "${url}/login"
 
-TOKEN=$(curl -b cookies.txt --cookie-jar cookies.txt -s -L ${URL}/login | pup '[name=nonce] attr{value}')
-curl -s -o /dev/null -b cookies.txt -c cookies.txt -F "nonce=${TOKEN}" -F "name=${NAME}" -F "password=${PASSWORD}" -H "Expect:" ${URL}/login
+    token=$(curl -b cookies.txt --cookie-jar cookies.txt -s -L "${url}/admin/teams" | pup '#update-user-modal [name=nonce] attr{value}')
+    curl -s -o /dev/null -b cookies.txt -c cookies.txt -F "nonce=${token}" -F "name=${team}" -F "email=${mail}" -F "password=${credentials}" -F "id=new" -F "website=" -F "affiliation=" -F "country="  -H "Expect:" "${url}/admin/team/new"
 
-TOKEN=$(curl -b cookies.txt --cookie-jar cookies.txt -s -L ${URL}/admin/teams | pup '#update-user-modal [name=nonce] attr{value}')
-curl -s -o /dev/null -b cookies.txt -c cookies.txt -F "nonce=${TOKEN}" -F "name=${TEAM}" -F "email=${MAIL}" -F "password=${CREDENTIALS}" -F "id=new" -F "website=" -F "affiliation=" -F "country="  -H "Expect:" ${URL}/admin/team/new
+    echo "team ${team} has been added."
+}
 
-echo "team ${TEAM} has been added."
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+    addTeam "$1" "$2" "$3" "$4" "$5" "$6"
+fi
